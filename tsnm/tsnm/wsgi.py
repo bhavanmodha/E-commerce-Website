443c66042c33d900827ebd6bc5ce45dd94d8f7e3
bhava <bhavanmodha@gmail.com>
2021-06-10 12:27:09 +0530

    E-commerce Website

---
+++ tsnm/tsnm/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for tsnm project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tsnm.settings')
+
+application = get_wsgi_application()
